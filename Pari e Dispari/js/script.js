0fe137e3227a77f7cde48366ab680d6fd587d075
dpalexandru <alexpoian91@hotmail.com>
2025-07-07 16:34:32 +0200

    feat: sum pc and user numbers

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -4,11 +4,17 @@ const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"))
 
 console.log(`Hai scelto ${pariODispari} e il numero ${numeroUtente}`)
 
-// generiamo numero random per computer e stampiamo per test
+// generiamo numero random per computer 
 function numeroRandom() {
   const numeroCasuale = Math.floor(Math.random() * 5) + 1;
   return numeroCasuale;
 }
 
+//stampiamo per test
 const numeroPc = numeroRandom();
-console.log(numeroPc);
+console.log("Il numero dell'avversario è: ",numeroPc);
+
+//sommiamo i due numeri 
+const totale = numeroUtente + numeroPc;
+console.log("Il totale delle vostre giocate è: ",totale);
+
