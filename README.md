5acae6c2dc19f354a34cbcc5d15dbfbad40325fa
dpalexandru <alexpoian91@hotmail.com>
2025-07-07 16:01:42 +0200

    feat print final resultat of control

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Esercizio
 
-Palidroma
+Palindroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // chiedo all'utente di inserire una parola e creo la stringa 
 const parolaInserita = prompt("Inserisci uan parola, ti dirò se è palidroma");
-console.log("Parola invertita:", parolaInserita);
+console.log("Parola inserita:", parolaInserita);
 
 function controlloPalindroma() {
   const inputInArray = parolaInserita.split('');
@@ -11,3 +11,12 @@ function controlloPalindroma() {
 //chaimo la funzione + test
 const parolaInvertita = controlloPalindroma().join('');
 console.log("Parola invertita:", parolaInvertita);
+
+
+// stampo il risultato 
+if (parolaInserita === parolaInvertita) {
+  console.log("La parola inserita è Palindroma.")
+} else {
+  console.log("La parola inserita non è Palindroma.")
+
+}
