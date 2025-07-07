16331f5d459a8523726a4a54b87c51fa6697f54f
dpalexandru <alexpoian91@hotmail.com>
2025-07-07 16:24:57 +0200

    feat: ask input from usera

---
+++ Pari e Dispari/js/script.js
@@ -0,0 +1,5 @@
+//Chiediamo all' utente pari o dispari e numero
+const pariODispari = prompt("Scegli pari o dispari")
+const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 5"))
+
+console.log(`Hai scelto ${pariODispari} e il numero ${numeroUtente}`)
