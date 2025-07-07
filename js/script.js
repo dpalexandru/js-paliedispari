7ce249de3ed546fd096c429c366ff4edf34c1a5a
dpalexandru <alexpoian91@hotmail.com>
2025-07-07 15:58:09 +0200

    fix: function to reverse a string

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 // chiedo all'utente di inserire una parola e creo la stringa 
 const parolaInserita = prompt("Inserisci uan parola, ti dirò se è palidroma");
-console.log(parolaInserita);
+console.log("Parola invertita:", parolaInserita);
 
-function controlloPalidroma() {
-  const inversoParola = parolaInserita.reverse();
+function controlloPalindroma() {
+  const inputInArray = parolaInserita.split('');
+  const inversoParola = inputInArray.reverse()
   return inversoParola;
-}
-controlloPalidroma();
-console.log(inversoParola);
+};
+
+//chaimo la funzione + test
+const parolaInvertita = controlloPalindroma().join('');
+console.log("Parola invertita:", parolaInvertita);
